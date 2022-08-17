34d02592143097e02dd51046be8eb93e721ff099
ocampoad <ocampo.adriane13@gmail.com>
2022-08-16 21:45:02 -0700

    fixed routes

--- controllers/apiRoutes/blogRoutes/index.js
+++ controllers/apiRoutes/blogRoutes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const connection = require('./../../../config/connection');
 
 router.get('/', async (req, res) => {
-    console.log('i got hit!')
+    
 })
 
 module.exports = router;
